6fc1d535f828b20ce972fc292aee5caa6226ecd1
Magesh Kuppan <tkmagesh77@gmail.com>
2022-09-12 14:11:52 +0530

    program structure

---
+++ 01-intro/hello-world.go
@@ -0,0 +1,16 @@
+//Package declaration
+package main
+
+//import dependent packages
+import "fmt"
+
+//package level declarations
+
+//package init function(s)
+
+//main function
+func main() {
+	fmt.Println("Hello World!")
+}
+
+//other functions
