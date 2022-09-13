6be453bb6ad9cf347ba7ba5223fe4cd033fd5665
Magesh Kuppan <tkmagesh77@gmail.com>
2022-09-13 14:34:30 +0530

    converting a panic into an error

---
+++ 06-pacnic-recovery/02-demo.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	quotient, remainder, err := divideClient(100, 0)
+	if err != nil {
+		fmt.Println("Divide operation failed")
+		fmt.Println("Perform an alternate operation")
+	} else {
+		fmt.Println(quotient, remainder)
+	}
+}
+
+func divideClient(x, y int) (quotient, remainder int, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			err = e.(error)
+			return
+		}
+	}()
+	quotient, remainder = divide(x, y)
+	return
+}
+
+//from a 3rd party library
+func divide(x, y int) (quotient, remainder int) {
+	quotient, remainder = x/y, x%y
+	fmt.Println("after calculation")
+	return
+}
