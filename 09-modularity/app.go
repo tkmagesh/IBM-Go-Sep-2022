df203ed61e3d0db95d748289e20dcc3823b67604
Magesh Kuppan <tkmagesh77@gmail.com>
2022-09-14 11:14:11 +0530

    using 3rd party packages

--- 09-modularity/app.go
+++ 09-modularity/app.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"modularity-demo/calculator"
+	"modularity-demo/calculator/utils"
+
+	"github.com/fatih/color"
 )
 
 func main() {
-	fmt.Println("Module executed")
+	//fmt.Println("Module executed")
+	color.Red("Module executed")
+
 	fmt.Println(calculator.Add(100, 200))
 	fmt.Println(calculator.Subtract(100, 200))
 	fmt.Println("OpCount =", calculator.OpCount())
+	fmt.Println(utils.IsEven(11))
 }
